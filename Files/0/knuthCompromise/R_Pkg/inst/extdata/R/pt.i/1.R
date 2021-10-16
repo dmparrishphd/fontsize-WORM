4c29227fb0712437291bcd2420a87a921ca12f5a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-10-16 16:30:44 -0400

    Update 1.R

--- Files/0/knuthCompromise/R_Pkg/inst/extdata/R/pt.i/1.R
+++ Files/0/knuthCompromise/R_Pkg/inst/extdata/R/pt.i/1.R
@@ -1,4 +1,4 @@
-pt.i <- function ( k ) {
+pt.i <- function ( i ) {
 	MAX <- 2 ^ .Machine $ double.digits - 1
-	y <- ( 5 : 9 ) [ ( k - 1 ) %% 5 + 1 ] * 2 ^ ( ( k - 1 ) %/% 5 )
+	y <- ( 5 : 9 ) [ ( i - 1 ) %% 5 + 1 ] * 2 ^ ( ( i - 1 ) %/% 5 )
 	if ( ! is.finite ( y ) || MAX < y ) NA_real_ else y }
